da61c0e352b0811c4d80d78c7916138e721a64d9
rushikeshtreb <rushikeshvadnal7@gmail.com>
2024-07-17 12:21:57 +0530

    message

--- src/Components/ChatView.jsx
+++ src/Components/ChatView.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useContext } from "react";
-import { ChatHeader, Message, Wrapper } from "../Wrappers/ChatView";
+import {
+  ChatHeader,
+  Message,
+  Wrapper,
+  TypeMessage,
+} from "../Wrappers/ChatView";
 import Dark_theme_chat from "../assets/chat-theme-dark.jpeg";
 import { ChatContext } from "../Context/ChatContext";
 
@@ -26,10 +31,30 @@ const ChatView = () => {
 
         <Message>
           {selectedChat.message.map((message) => {
-            console.log(message)
+            console.log(message);
             return <div className="message">{message}</div>;
-        })}
+          })}
         </Message>
+
+        <TypeMessage>
+          <div className="sticker-svg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+              />
+            </svg>
+          </div>
+          <input type="text" className="input-text" />
+        </TypeMessage>
       </Wrapper>
     </>
   );
--- src/Wrappers/ChatView.js
+++ src/Wrappers/ChatView.js
@@ -1,93 +1,128 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.section`
-    width: 100vw;
-    height: 100vh;
-    height: calc(100vh - 4rem);
-    margin: 4rem 0px;
-    background-repeat: repeat;
+  width: 100vw;
+  height: 100vh;
+  height: calc(100vh - 4rem);
+  margin: 4rem 0px;
+  background-repeat: repeat;
 
-    @media (min-width: 900px) {
-        width: 100%;
-        height: 100%;
-    }
-`
+  @media (min-width: 900px) {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export const ChatHeader = styled.div`
+  width: 100%;
+  height: 4rem;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 1000;
+  background-color: var(--gray);
+  display: flex;
+  padding: 5px 20px;
 
-    width: 100%;
-    height: 4rem;
-    position: fixed;
-    top: 0;
-    right: 0;
-    z-index: 1000;
-    background-color: var(--gray);
+  .profile-image {
+    width: 10%;
     display: flex;
-    padding: 5px 20px;
+    justify-content: start;
+    align-items: center;
+  }
+  .profile-image div {
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    color: var(--blue);
+    width: 2.5rem;
+    height: 2.5rem;
+    background-color: aliceblue;
+  }
 
-    .profile-image{
-        width: 10%;
-        display: flex;
-        justify-content: start;
-        align-items: center;
-    }
-    .profile-image div{
-        border-radius: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 2rem;
-        color: var(--blue);
+  h1 {
+      width: 90%;
+    padding: 0px 1rem;
+    font-size: 1.5rem;
+    text-transform: capitalize;
+  }
+  
+  @media (min-width: 900px) {
+      width: 65%;
+      .profile-image div{
         width: 3rem;
         height: 3rem;
-        background-color: aliceblue;
     }
-
-    h1{
-        width: 90%;
-        font-size: 1.5rem;
-        text-transform: capitalize;
-    }
-
-    @media (min-width: 900px) {
-        width: 65%;
-    }
-`
+  }
+`;
 
 export const Message = styled.div`
-    height: calc(100vh - 8rem);
-    margin: 4rem 0px 4rem 4rem;
-    overflow-y: scroll;
-    overflow-x: hidden;
-    display: flex;
-    flex-direction: column-reverse;
+  height: calc(100vh - 8rem);
+  margin: 4rem 0px 4rem 4rem;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  display: flex;
+  flex-direction: column-reverse;
 
+  .message {
+    width: max-content;
+    max-width: 70%;
+    background-color: var(--gray);
+    margin: 8px;
+    border-radius: 5px;
+    padding: 1rem;
+    color: white;
+  }
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
 
-    .message{
-        width: max-content;
-        max-width: 70%;
-        background-color: var(--gray);
-        margin: 8px;
-        border-radius: 5px;
-        padding: 1rem;
-        color: white;
-    }
-    &::-webkit-scrollbar {
-        width: 5px;
-    }
+  &::-webkit-scrollbar-track {
+    background: transparent;
+    border-radius: 10px;
+  }
 
-    &::-webkit-scrollbar-track {
-        background: transparent;
-        border-radius: 10px;
-    }
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--primary);
+    border-radius: 10px;
+    border: 3px solid rgb(0, 0, 0, 0.5);
+  }
 
-    &::-webkit-scrollbar-thumb {
-        background-color: var(--primary);
-        border-radius: 10px;
-        border: 3px solid rgb(0,0,0,0.5);
-    }
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: var(--secondary);
+  }
+`;
 
-    &::-webkit-scrollbar-thumb:hover {
-        background-color: var(--secondary);
-    }
-`
+export const TypeMessage = styled.div`
+  width: max-content;
+  position: fixed;
+  bottom: 10px;
+  padding: 0px 0.5rem;
+  z-index: 1000;
+  left: 50%;
+    transform: translateX(-50%);
+  background-color: var(--gray);
+  color: white;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
+  /* box-shadow: 0 0 5px rgba(0, 0, 0, 0.5); */
+
+  .input-text {
+    background-color: var(--gray);
+    width: 15rem;
+    padding: 0.5rem;
+}
+
+  input:focus {
+    outline: none;
+    border: none;
+}
+@media (min-width: 900px) {
+    width: 25rem;
+    left: 50%;
+  }
+`;
